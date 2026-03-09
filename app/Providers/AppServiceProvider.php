44771ad2deed525fa95e4487860e5c8330a7b09a
Ciro Pacheco <91978029+ciropg@users.noreply.github.com>
2026-03-09 12:24:30 -0500

    Force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::policy(Budget::class, BudgetPolicy::class);
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
